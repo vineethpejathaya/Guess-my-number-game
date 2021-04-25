18581b829fda73d9ca3b5b1da08b575f1180aece
vineethpejathaya <vineethpejathaya@gmail.com>
2021-04-25 19:15:46 +0530

    added js code

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+'use strict';
+let upper = 20;
+let lower = 1;
+let secNumber = Math.floor(Math.random()* (upper - lower +1)+lower);
+console.log(secNumber);
+const message = document.querySelector(".start-message");
+const checkClick = document.querySelector("button");
+const input = document.querySelector('input');
+const screenNumber = document.querySelector(".screen-number");
+checkClick.addEventListener('click',function(){
+let inputNumber = Number(input.value);
+console.log(inputNumber);
+if(inputNumber === secNumber)
+{
+   message.textContent = "congratulations !";
+}
+else if (inputNumber < secNumber)
+{
+    message.textContent = "you are too low !";
+}
+else{
+    message.textContent = "you are too high !";
+}
+})
+screenNumber.textContent = secNumber;
+
+
 
